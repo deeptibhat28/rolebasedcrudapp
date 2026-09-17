a788885ce7baf57a61034568eb3b284caa5ccbc7
deeptibhat28 <138190658+deeptibhat28@users.noreply.github.com>
2026-09-17 18:01:06 +0530

    edit some code.

--- src/pages/AdminSubmissions.jsx
+++ src/pages/AdminSubmissions.jsx
@@ -424,7 +424,7 @@ export default function AdminSubmissions() {
           <ThemeToggle />
           <button
             onClick={() => navigate("/admin-dashboard")}
-            className="px-4 py-2 bg-blue-50 text-blue-700 border border-blue-200 dark:bg-[#1b082d]/70 dark:text-purple-200 dark:border-purple-500/45 rounded-xl font-bold text-xs sm:text-sm hover:bg-blue-100 hover:border-blue-300 dark:hover:bg-[#1b082d] transition duration-200 shadow-md"
+            className="px-4 py-2 bg-blue-50 text-blue-700 border border-blue-200 dark:bg-[#1b082d]/70 dark:text-purple-200 dark:border-purple-500/45 rounded-xl font-bold text-xs sm:text-sm hover:bg-blue-100  dark:hover:bg-[#1b082d] transition duration-200 shadow-md"
           >
             Dashboard
           </button>
